3ab124c1e116655799c3466c8c3b3307b2ad062f
Yarrow Martin <yarrowmartin3@gmail.com>
2025-10-22 01:46:04 -0400

    fix: erreur addEventListener corrigée

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Script chargé ✅");
+
+  const form = document.getElementById("contactForm");
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Formulaire envoyé !");
+    });
+  }
 });
